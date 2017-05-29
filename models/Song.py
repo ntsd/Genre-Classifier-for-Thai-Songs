34005d9950db0a26571e056c1c7d069c2adc879c
Jiravat Boonkumnerd <jo06942@gmail.com>
2017-05-29 13:58:45 +0700

    up

--- models/Song.py
+++ models/Song.py
@@ -1,21 +1,21 @@
 class Song:
-    def __init__(self, title, artist, album, record, emotion, genre):
+    def __init__(self, title, artist, album, record, category, genre):
         self.title = title
         self.artist = artist
         self.album = album
         self.record = record
-        self.emotion = emotion
+        self.category = category
         self.genre = genre
 
     def __str__(self):
-        return str([self.title, self.artist, self.album, self.record, self.emotion, self.genre])
+        return str([self.title, self.artist, self.album, self.record, self.category, self.genre])
 
     def getDetailCSV(self):
-        return [self.title, self.artist, self.album, self.record, self.emotion, self.genre]
+        return [self.title, self.artist, self.album, self.record, self.category, self.genre]
 
     def __repr__(self):
         return self.title
 
     def __iter__(self):
-        return iter([self.title, self.artist, self.album, self.record, self.emotion, self.genre])
+        return iter([self.title, self.artist, self.album, self.record, self.category, self.genre])
 
